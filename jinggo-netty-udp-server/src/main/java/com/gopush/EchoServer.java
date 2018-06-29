5aec956ebb9a6d21899f0b4c22afe74244a8ba4e
wangyj <wangyj@soonfor.com>
2018-06-29 16:29:07 +0800

    netty upd demo

---
+++ jinggo-netty-udp-server/src/main/java/com/gopush/EchoServer.java
@@ -0,0 +1,13 @@
+package com.gopush;
+
+/**
+ * @author wangyj
+ * @description
+ * @create 2018-06-29 16:24
+ **/
+public class EchoServer {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ jinggo-netty-udp-server/src/main/java/com/gopush/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by gz12 on 2018-06-29.
+ */
+package com.gopush;
